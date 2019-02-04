4ea8b563ffd10786f507ed5530bcaefdc18cffd9
Thomas Dubernet <thoms.dubernet@gmail.com>
2019-02-04 14:54:29 +0100

    add formulaire

---
+++ inc/inc_login.php
@@ -0,0 +1,14 @@
+
+
+
+<form action="inc/login-post.php" method="post" class=" w-50 m-auto text-center" style="padding-top: 100px;">
+    <div class="form-group">
+        <label for="my-input">Email Adress</label><br>
+        <input id="my-input" class="form-control" type="mail" placeholder="Enter mail" name="pseudo">
+    </div>
+    <div class="form-group">
+        <label for="my-input">Password</label>
+        <input id="my-input" class="form-control" type="password" placeholder="password" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Connecter</button>
+</form>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("inc/inc_head.php"); ?>
+</head>
+<body>
+    <header class="text-center">
+        <h1>Mon premier authentificator</h1>
+    </header>
+    
+    <section id="login">
+    <?php include_once('inc/inc_login.php'); ?>
+    </section>
+
+   
+<!-- Scripts -->
+<?php include_once("inc/inc_scripts.php"); ?>
+</body>
+</html>
