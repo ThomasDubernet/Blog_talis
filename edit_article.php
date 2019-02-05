bd5fceaa0c5bd746eb1cb5b5ed3e8b5ab6475fb5
Thomas Dubernet <thoms.dubernet@gmail.com>
2019-02-05 18:10:56 +0100

    debut update mais delete all database

--- edit_article.php
+++ edit_article.php
@@ -1,6 +1,7 @@
 <?php include_once("inc/inc_head.php"); 
 $req_row = $db->prepare("SELECT * FROM posts");
 $req_row->execute();
+$row = $req_row->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <body>
@@ -13,9 +14,10 @@ $req_row->execute();
     <section class="text-center" id="form_edit">
     <form action="inc/update.php">
         <label for="">Titre</label><br>
-        <input type="text" name="title_article"><br>
-        <label for="">Titre</label><br>
-        <textarea name="content_article" cols="30" rows="10"><?php echo $row["content"];?></textarea>
+        <input type="text" name="title_article" placeholder="<?php echo $row["title"];?>"><br>
+        <label for="">Message</label><br>
+        <textarea name="content_article" cols="30" rows="10"><?php echo $row["content"];?></textarea><br>
+        <input type="submit">
     </form>
     </section>
 
--- inc/update.php
+++ inc/update.php
@@ -5,6 +5,10 @@ $title_article = $_POST["title_article"];
 $content_article = $_POST["content_article"];
 
 
-$req = $db->prepare("UPDATE posts SET title= ");
-$req->execute();
+$req = $db->prepare("UPDATE posts SET title= :title_article, content= :content_article ");
+$req->execute(array(
+    "title_article" => $title_article,
+    "content_article" => $content_article
+));
+header('Location: admin.php?message=Succes for update');
 ?>
