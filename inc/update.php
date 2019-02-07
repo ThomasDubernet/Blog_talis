90c779f6db2e6ce6caccb2d1d152adb891bdbc1b
Thomas Dubernet <thoms.dubernet@gmail.com>
2019-02-07 09:05:18 +0100

    Update update.php

--- inc/update.php
+++ inc/update.php
@@ -1,14 +1,11 @@
 <?php
-require('database.php');
-$numero_id = $_GET["id"];
-$title_article = $_POST["title_article"];
-$content_article = $_POST["content_article"];
+session_start();
+require "database.php";
+$title_article =$_GET["title_article"];
+$content_article =$_GET["content_article"];
 
 
-$req = $db->prepare("UPDATE posts SET title= :title_article, content= :content_article ");
-$req->execute(array(
-    "title_article" => $title_article,
-    "content_article" => $content_article
-));
+$req = PDO::prepare("UPDATE posts SET title= '$title_article', content= '$content_article' ");
+$req_modif-> query($req);
 header('Location: admin.php?message=Succes for update');
 ?>
